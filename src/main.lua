b9ea5957e474d70ca2ff42a1ef13d69b2eb8cd82
Jehadel <jean.delpech@gmail.com>
2023-12-09 23:13:47 +0100

    Feat/ Add cohesion force + parameters adjustment

--- src/main.lua
+++ src/main.lua
@@ -8,14 +8,17 @@
 -- Constants
 W_WIDTH = 800
 W_HEIGHT = 600
-W_LIMIT = 50
+W_LIMIT = 80
 
-N_BOIDS = 100
-CVISUAL_RANGE = 75 -- could be an individual boid property
-VMAX = 100
+N_BOIDS = 80
+CVISUAL_RANGE = 60 -- could be an individual boid property
 V_TURN = 2 -- could be an individual boid property
-AVOIDANCE = .1
-MINDISTANCE = 50
+MINDISTANCE = 20
+VMAX = 100
+
+AVOIDANCE = 20 
+COHESION = 5 
+CENTERING = 200
 
 -- boids table
 local boids = {}
@@ -47,46 +50,83 @@ function createBoid()
 
 end
 
-function keepDistance(pBoid, pMinDistance, pAvoidance)
 
+function cohesion(pBoid, pVisualRange)
+
+  local delta = {}
   local dVx = 0
   local dVy = 0
-  
+  local nearBoids = {}
+  local sumX = 0
+  local sumY = 0
+  local sumVx = 0
+  local sumVy = 0
+  local n = 0
+
   for index, otherBoid in ipairs(boids.list) do
-    if pBoid ~= otherBoid then
-      if distance(otherBoid, pBoid) < pMinDistance then
-        dVx = dVx + (pBoid.x - otherBoid.x)
-        dVy = dVy + (pBoid.y - otherBoid.y)
-      end
+
+    if distance(pBoid, otherBoid) < pVisualRange then
+      sumX = sumX + otherBoid.x
+      sumY = sumY + otherBoid.y
+      sumVx = sumVx + otherBoid.vx
+      sumVy = sumVy + otherBoid.vy
+      n = n + 1
     end
   end
 
-  return {dVx * pAvoidance, dVy * pAvoidance}
+  delta.dx = sumX/n - pBoid.x
+  delta.dy = sumY/n - pBoid.y
+  delta.dVx = sumVx/n - pBoid.vx 
+  delta.dVy = sumVy/n - pBoid.vy
+  
+  return delta
 
 end
 
 
-function keepInside(pBoid)
-  local dVx = 0
-  local dVy = 0
+function keepDistance(pBoid, pMinDistance)
 
-  if pBoid.x < W_LIMIT then
-    dVx = V_TURN 
+  local dist = {}
+  dist.dx = 0
+  dist.dy = 0
+  
+  for index, otherBoid in ipairs(boids.list) do
+    if pBoid ~= otherBoid then
+      if distance(otherBoid, pBoid) < pMinDistance then
+        dist.dx = dist.dx + (pBoid.x - otherBoid.x)
+        dist.dy = dist.dy + (pBoid.y - otherBoid.y)
+      end
+    end
   end
 
-  if pBoid.x > W_WIDTH - W_LIMIT then
-    dVx = - V_TURN
+  return dist 
+
+end
+
+
+function keepInside(pBoid, pVTurn, pLimit)
+
+  local turn = {}
+  turn.dVx = 0
+  turn.dVy = 0
+
+  if pBoid.x < pLimit then
+    turn.dVx = pVTurn
   end
 
-  if pBoid.y < W_LIMIT then
-    dVy = V_TURN
+  if pBoid.x > W_WIDTH - pLimit then
+    turn.dVx = - pVTurn 
   end
 
-  if pBoid.y > W_HEIGHT - W_LIMIT then
-    dVy = - V_TURN
+  if pBoid.y < pLimit then
+    turn.dVy = pVTurn 
   end
 
-  return {dVx, dVy}
+  if pBoid.y > W_HEIGHT - pLimit then
+    turn.dVy = - pVTurn 
+  end
+
+  return turn 
 
 end
 
@@ -124,19 +164,26 @@ function love.update(dt)
 
   for index, boid in ipairs(boids.list) do 
 
-    -- cohesion()
-    -- align()
+    -- align position and speed with that of others
+    cohesionForce = cohesion(boid, CVISUAL_RANGE)
     -- boids avoid each other
-    avoidanceForce = keepDistance(boid, MINDISTANCE, AVOIDANCE)
+    avoidanceForce = keepDistance(boid, MINDISTANCE)
     -- boids return to the center when approching window’s edges
-    centeringForce = keepInside(boid)
+    centeringForce = keepInside(boid, V_TURN, W_LIMIT)
 
     -- boids speed adjustement according all forces
     -- we could add ponderations
-    boid.vx = boid.vx + avoidanceForce[1] 
-                      + centeringForce[1]
-    boid.vy = boid.vy + avoidanceForce[2] 
-                      + centeringForce[2]
+    boid.vx = boid.vx + (avoidanceForce.dx * AVOIDANCE
+                          + centeringForce.dVx * CENTERING
+                          + (cohesionForce.dx 
+                            + cohesionForce.dVx) * COHESION
+                        ) * dt
+
+    boid.vy = boid.vy + (avoidanceForce.dy * AVOIDANCE
+                        + centeringForce.dVy * CENTERING
+                        + (cohesionForce.dy 
+                          + cohesionForce.dVy) * COHESION
+                        ) * dt
 
     -- speed limitation
     if math.abs(boid.vx) > VMAX then
@@ -162,7 +209,7 @@ end
 function love.draw()
 
   for index, boid in ipairs(boids.list) do
-    love.graphics.draw(boids.img, boid.x, boid.y, -math.atan2(boid.vx, boid.vy), 1, 1, boids.w/2, boids.h/2)
+    love.graphics.draw(boids.img, boid.x, boid.y, -math.atan2(boid.vx, boid.vy), .67, .67, boids.w/2, boids.h/2)
   end
 
 end
